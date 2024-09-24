67b5674558099f244d542fa0885bcec072c96c43
Dawei84 <dwhao84@gmail.com>
2024-09-24 18:42:31 +0800

    Add quiz_brain of class, for organize function.

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,8 +1,16 @@
-# TODO: Asking the questions
+from main import question
 
 
-# TODO: Checking if the answer was correct
+class Quiz_brain:
+    # TODO: Asking the questions
+    def __init__(self, question_number, question_list):
+        self.question_number = 0
 
+    # TODO: Checking if the answer was correct
+    def check_answer(self):
+        print("Active the check answer")
 
-# TODO: Checking if we're the end of the quiz.
-
+    # TODO: Checking if we're the end of the quiz.
+    def next_question(self, question_number):
+        question_number += 1
+        print(question_number)
